05db71a75a426adb9de4f67bc395745ba7eeedd6
Artyom Maisiuk <artyom.majsyuk@gmail.com>
2023-11-05 18:03:01 +0300

    client: ready to deploy

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import {store, persistor} from "./store";
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 ReactDOM.render(
     <BrowserRouter>
--- client/src/pages/LevelOne/LevelOne.jsx
+++ client/src/pages/LevelOne/LevelOne.jsx
@@ -1,11 +1,9 @@
 
-import React, {useState} from 'react';
+import React from 'react';
 import { Header } from '../../components/Header';
 import { MainLevel } from '../../components/MainLevel';
 import Footer from '../../components/Footer';
 import "./LevelOne.css";
-
-
 
 
 const LevelOne = () => {
