6cfcb7e696f41c6516a000b4b6809ee2dcb9ccfd
Artyom Maisiuk <artyom.majsyuk@gmail.com>
2023-11-27 22:48:30 +0300

    added chat icon

--- client/src/components/SupportEngine/chat.js
+++ client/src/components/SupportEngine/chat.js
@@ -5,10 +5,10 @@ export const styles = {
         // Border
         borderRadius: '50%',
         // Background 
-        backgroundImage: `url(/images/red_bgr.jpg)`, 
+        backgroundImage: `url(/images/chat.png)`, 
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
-        backgroundSize: '200px',
+        backgroundSize: '84px',
         // Size
         width: '84px',
         height: '84px',
