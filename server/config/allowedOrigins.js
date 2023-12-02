8e83849aa6ab2b4b73b60adc89278c6eb8230291
Artyom Maisiuk <artyom.majsyuk@gmail.com>
2023-12-02 16:28:14 +0300

    fixed CORS

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'https://visainusa.onrender.com',
     'https://visainusa.vercel.app',
+     process.env.URL_CLIENT,
     'http://localhost:3500',
     'http://localhost:3000'
 ];
