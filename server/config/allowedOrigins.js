bd1f7ea3e62e4ef5b8bd01c226aa4c4b7f477fcf
Artyom Maisiuk <artyom.majsyuk@gmail.com>
2023-12-09 10:39:12 +0300

    cors fix

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-    process.env.URL_CLIENT,
-    "http://localhost:3000"
+    process.env.URL_CLIENT
+    // "http://localhost:3000"
 ];
 
 module.exports = allowedOrigins;
