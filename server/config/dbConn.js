5d4577ac4d4c19032ba7c60b03cc9e07ffa1501d
Olya2125 <139589012+Olya2125@users.noreply.github.com>
2024-02-29 12:13:27 +0300

    changed tables

--- server/config/dbConn.js
+++ server/config/dbConn.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URI, {
+    // try {
+    //     await mongoose.connect(process.env.DATABASE_URI, {
             
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
-    } catch (err) {
-        console.error(err);
-    }
+    //         useUnifiedTopology: true,
+    //         useNewUrlParser: true
+    //     });
+    // } catch (err) {
+    //     console.error(err);
+    // }
 }
 
 module.exports = connectDB
